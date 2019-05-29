4c9cd323bc5ae1d5116ad47e12f8afd83cc286a8
zhusitao <zhusitao1990@163.com>
2019-05-29 16:56:47 +0800

    modift loop

--- sRNA_annot/catalog.py
+++ sRNA_annot/catalog.py
@@ -1,7 +1,8 @@
 #-*-coding:utf-8-*-
-import os
+import os,re
 import sys
 from optparse import OptionParser
+from biosoftware import samtools
 
 
 def main():
@@ -18,8 +19,60 @@ def main():
         print('\033[0;31;40m%s\033[0m' % "Warning: database, prefix, mismatch and novel_threshold must be given\n")
         sys.exit(parser.print_help())
 
+
+
+
+def reverse_complement(seq:str):
+    COMPLEMENT_TRANS = str.maketrans('TAGCtagc', 'ATCGATCG')
+    return seq[::-1].translate(COMPLEMENT_TRANS)
+
+def annot_ancrna(file_handle,database,prefix):
+    des = {} # miRNA description file
+    aln = {} # aligner bam dict
+    dat = {}
+    # 处理数据库中的miRNA.des文件，存入字典
+    if os.path.exists(database+"/d_sncrna/miRNA.des"):
+        with open(database+"/d_sncrna/miRNA.des",'r') as F:
+            for line in F.readlines():
+                line = line.strip()
+                if line.startswith('#'):continue
+                tmp = line.split('\t')
+                des[tmp[0]] = tmp[1]
+    # 处理sncrna比对bam文件
+    bam = os.popen("{samtools} view {prefix}_sncrna.bam".format(samtools=samtools(),prefix=prefix))
+    for line in bam.readlines():
+        line = line.strip()
+        if line.startswith("@"):continue
+        a = line.split()
+        MD= 0
+        if re.match('MD:Z:(\S+)',line):
+            MD = re.match('MD:Z:(\S+)',line).group(1)
+        if tmp[1] == '16':
+            tmp[9] = reverse_complement(tmp[9]) # 反向互补负链
+        tmp[9].replace('^CCCCC','').replace('GGGGG$','') # 除去加上的碱基
+        if tmp[9] in aln.keys():
+            dat[aln[tmp[9]]]
+
+
+        dat[tmp[2]] += 1
+
+        aln[tmp[9]] = tmp[2]
+
+
+
+
+
 def catalog(database,prefix,mimatch,novel):
-	OA = open(prefix+".catalog.xls",'w')
-	OA.writelines("sRNA id\tCount\tType\tDescription\n")
+    OA = open(prefix+".catalog.xls",'w')
+    OA.writelines("sRNA id\tCount\tType\tDescription\n")
+    annot_ancrna(OA,database,prefix)
+
+
+
+
+
+def annot_rfam():
+    pass
+
 
 
